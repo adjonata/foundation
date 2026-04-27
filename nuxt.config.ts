a201ad89fcd75e17a834448c88530fb20af6d1bf
adjonata <djonataalex@gmail.com>
2026-04-26 23:31:22 -0300

    feat: added new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,14 @@ export default defineNuxtConfig({
 
   css: ['~/assets/css/main.css'],
 
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' }
+      ]
+    }
+  },
+
   routeRules: {
     '/': { prerender: true }
   },
